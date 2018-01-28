54442731a147be96ddf3d6e5dd908e1b57b47f8a
c0deBym3ta <kalimetasploiter@gmail.com>
2018-01-28 16:09:02 +0100

    Implementing Constructor and pop, push function.

--- TemplateClass/Stack.cpp
+++ TemplateClass/Stack.cpp
@@ -1,12 +1,27 @@
 #include "Stack.h"
 
 
-
-Stack::Stack()
-{
+template<typename T>
+Stack<T>::Stack(int n){
+	size = n > 0 ? n : 10;
+	top = -1;
+	stackPtr = new T[size];
 }
 
+template<typename T>
+bool Stack<T>::push(const T& pushValue) {
+	if (!isFull()) {
+		stackPtr[++top] == pushValue;
+		return true;
+	}
+	return false;
+}
 
-Stack::~Stack()
-{
+template<typename T>
+bool Stack<T>::pop(T& popValue) {
+	if (!isEmpty()) {
+		popValue = stackPtr[top--];
+		return true;
+	}
+	return false;
 }
--- TemplateClass/Stack.h
+++ TemplateClass/Stack.h
@@ -2,9 +2,26 @@
 
 #include "header.h"
 
+template<typename T>
 class Stack{
 public:
-	Stack();
-	~Stack();
+	Stack(int n = 10);
+	~Stack() {
+		delete stackPtr;
+	}
+	bool push(const T& pushValue);
+	bool pop(T& popValue);
+
+private:
+	int size;
+	int top;
+	T *stackPtr;
+	
+	bool isEmpty() const{
+		return (top == -1);
+	}
+	bool isFull() const {
+		return (top == (size - 1));
+	}	
 };
 
